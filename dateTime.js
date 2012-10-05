91efebaefb1e741ecec1c2c2f0537468c739839d
Daemonl <damien@rebase.com.au>
2012-10-05 12:08:09 +1000

    Fixed AM-PM and added getDate and getTime

--- dateTime.js
+++ dateTime.js
@@ -30,7 +30,7 @@ define(['juration'], function(juration) {
 
     var datetime = function(input)
      {
-
+        var pri = {};
         var ts = undefined;
         var tz = undefined;
         if (input instanceof Object)
@@ -59,6 +59,8 @@ define(['juration'], function(juration) {
         this.ts = ts;
         
         this.dateObject = 0;
+
+        var ro = this;
         
         this.getJSON = function(){
           return {
@@ -88,12 +90,12 @@ define(['juration'], function(juration) {
           return this.ts;
         }
         
-        this.obj = function(){
-          if (this.dateObject == 0)
+        pri.obj = function(){
+          if (ro.dateObject == 0)
           {
-            this.dateObject = new Date((this.ts*1000)+(this.tz*1000));
+            ro.dateObject = new Date((ro.ts*1000)+(ro.tz*1000));
           }
-          return this.dateObject;
+          return ro.dateObject;
         };
         
         this.durationFrom = function(startTime)
@@ -102,10 +104,27 @@ define(['juration'], function(juration) {
           return ((tsdif>0)?'':'- ')+juration.stringify(Math.abs(tsdif));
         }
         
+        pri.tsDate = function()
+        {
+
+        }
+
+        this.getDate = function(){
+          var sn = {tz: this.tz, ts: 0};
+          sn.ts = (Math.floor((this.ts/1 + this.tz/1) / (60*60*24)) * (60*60*24)) - this.tz;
+          return new datetime(sn);
+        };
+
+        this.getTime = function(){
+          var sn = {tz: this.tz, ts: 0};
+          sn.ts = ((this.ts/1 + this.tz/1) % (60*60*24)) - this.tz;
+          return new datetime(sn);
+        };
+
         this.format = function(f){
-         // console.log(this.obj());
+         // console.log(pri.obj());
         //  this.dateObject = new Date(this.jsts);
-        //  return this.obj() + this.jsts;
+        //  return pri.obj() + this.jsts;
           switch (f)
           {
             case undefined: f = "d-m-Y"; break;
@@ -130,20 +149,20 @@ define(['juration'], function(juration) {
           {
         // Day
             // Day of the month, 2 digits with leading zeros
-            case "d": return this.leadingzero(this.obj().getUTCDate());
+            case "d": return this.leadingzero(pri.obj().getUTCDate());
 
             // A textual representation of a day, three letters
-            case "D": return EnumDaysOfTheWeek[this.obj().getUTCDay()][0];
+            case "D": return EnumDaysOfTheWeek[pri.obj().getUTCDay()][0];
 
             // Day of the month without leading zeros
-            case "j": return this.obj().getUTCDate();
+            case "j": return pri.obj().getUTCDate();
 
             // A full textual representation of the day of the week
-            case "l": return EnumDaysOfTheWeek[this.obj().getUTCDay()][1];
+            case "l": return EnumDaysOfTheWeek[pri.obj().getUTCDay()][1];
 
             // ISO-8601 numeric representation of the day of the week (added in PHP 5.1.0)
             // (Monday = 1, Sunday = 7)
-            case "N": return EnumDaysOfTheWeek[this.obj().getUTCDay()][2];
+            case "N": return EnumDaysOfTheWeek[pri.obj().getUTCDay()][2];
 
             // English ordinal suffix for the day of the month, 2 characters
             // st, nd, rd or th. Works well with j
@@ -151,7 +170,7 @@ define(['juration'], function(juration) {
 
             // Numeric representation of the day of the week
             // 0 (for Sunday) through 6 (for Saturday)
-            case "w": return this.obj().getUTCDay();
+            case "w": return pri.obj().getUTCDay();
 
             // The day of the year (starting from 0)
             case "z": return "z";
@@ -162,16 +181,16 @@ define(['juration'], function(juration) {
         // Month
 
             // A full textual representation of a month, such as January or March
-            case "F": return EnumMonths[this.obj().getUTCMonth()][1];
+            case "F": return EnumMonths[pri.obj().getUTCMonth()][1];
 
             // Numeric representation of a month, with leading zeros
-            case "m": return this.leadingzero(this.obj().getUTCMonth() + 1);
+            case "m": return pri.leadingzero(pri.obj().getUTCMonth() + 1);
 
             // A short textual representation of a month, three letters
-            case "M": return EnumMonths[this.obj().getUTCMonth()][0];
+            case "M": return EnumMonths[pri.obj().getUTCMonth()][0];
 
             // Numeric representation of a month, without leading zeros
-            case "n": return this.obj().getUTCMonth() + 1;
+            case "n": return pri.obj().getUTCMonth() + 1;
 
             // t  Number of days in the given month
             case "t": return 't';
@@ -184,52 +203,52 @@ define(['juration'], function(juration) {
             case "o": return 'o';
 
             // A full numeric representation of a year, 4 digits
-            case "Y": return this.obj().getUTCFullYear();
+            case "Y": return pri.obj().getUTCFullYear();
 
             // A two digit representation of a year
-            case "y": return (''+this.obj().getUTCFullYear()).substr(2,2);
+            case "y": return (''+pri.obj().getUTCFullYear()).substr(2,2);
 
         // Time
 
             // a Lowercase Ante meridiem and Post meridiem am or pm
             case "a":
-              var h = this.obj().getUTCHours();
-              return ((h > 12) || (h == 0))?"pm":"am";
+              var h = pri.obj().getUTCHours();
+              return (h > 11)?"pm":"am";
 
             // A Uppercase Ante meridiem and Post meridiem AM or PM
             case "A":
-              var h = this.obj().getUTCHours();
-              return ((h > 12) || (h == 0))?"PM":"AM";
+              var h = pri.obj().getUTCHours();
+              return (h > 11)?"PM":"AM";
 
             //B Swatch Internet time  000 through 999
             case "B": return 'B';
 
             //g 12-hour format of an hour without leading zeros 1 through 12
             case "g": 
-              var h = this.obj().getUTCHours() % 12;
+              var h = pri.obj().getUTCHours() % 12;
               return (h==0)?12:h;
 
             //G 24-hour format of an hour without leading zeros 0 through 23
-            case "G": return this.obj().getUTCHours();
+            case "G": return pri.obj().getUTCHours();
 
             //h 12-hour format of an hour with leading zeros  01 through 12
             case "h": 
-              var h = this.obj().getUTCHours() % 12;
+              var h = pri.obj().getUTCHours() % 12;
               return this.leadingzero((h==0)?12:h);
 
             //H 24-hour format of an hour with leading zeros  00 through 23
-            case "H": return this.leadingzero(this.obj().getUTCHours());
+            case "H": return this.leadingzero(pri.obj().getUTCHours());
 
             //i Minutes with leading zeros  00 to 59
-            case "i": return this.leadingzero(this.obj().getUTCMinutes());
+            case "i": return this.leadingzero(pri.obj().getUTCMinutes());
 
             //s Seconds, with leading zeros 00 through 59
-            case "s": return this.leadingzero(this.obj().getUTCSeconds());
+            case "s": return this.leadingzero(pri.obj().getUTCSeconds());
 
             //u  Microseconds (added in PHP 5.2.2). Note that date() will always generate 000000 since it takes an integer parameter, whereas DateTime::format() does support microseconds.
             case "u": return 0;
 
-            case "r": return this.obj();
+            case "r": return pri.obj();
           }
           return n;
         };
